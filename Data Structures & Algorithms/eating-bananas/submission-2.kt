1c21891e60efeae6c9fc5166b19d3c71ad7186de
Alex Lee <53023081+alexhlee6@users.noreply.github.com>
2026-07-20 01:04:33 -0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.kt
@@ -0,0 +1,38 @@
+class Solution {
+    fun minEatingSpeed(piles: IntArray, h: Int): Int {
+        piles.sort()
+
+        var lower = 1.0; var higher = piles[piles.size-1].toDouble()
+        while (lower < higher) {
+            val mid = ((lower+higher)/2).toInt()
+            val canFinish = canFinishBananas(piles.clone(), h, mid)
+            if (canFinish) {
+                higher = mid.toDouble()
+            } else {
+                lower = (mid+1).toDouble()
+            }
+        }
+        return higher.toInt()
+    }
+
+    fun canFinishBananas(piles: IntArray, h: Int, k: Int, idx: Int = 0): Boolean {
+        if (idx >= piles.size) {
+            return true
+        } else if (h <= 0) {
+            return false
+        } 
+        
+        val currCount = piles[idx]
+        if (currCount <= k) {
+            piles[idx] = 0
+            return canFinishBananas(piles, h-1, k, idx+1)
+        } else {
+            val requiredHours = ceil(piles[idx] / k.toDouble())
+            if (requiredHours <= h) {
+                piles[idx] = 0
+                return canFinishBananas(piles, h-requiredHours.toInt(), k, idx+1)
+            }
+            return false
+        }
+    }
+}
