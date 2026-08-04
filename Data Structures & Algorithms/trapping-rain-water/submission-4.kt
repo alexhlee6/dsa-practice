fb5be95a16dedb6a9b0c08e7b4a32df55ed2a765
Alex Lee <53023081+alexhlee6@users.noreply.github.com>
2026-08-03 23:02:56 -0700

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun trap(heights: IntArray): Int {
+        var totalWater = 0
+        val maxWaterRTL = heights.clone()
+        val maxWaterLTR = heights.clone()
+
+        for (i in 1 until heights.size) {
+            maxWaterLTR[i] = max(maxWaterLTR[i], maxWaterLTR[i-1])
+            maxWaterRTL[heights.size-1-i] = max(maxWaterRTL[heights.size-1-i], maxWaterRTL[heights.size-i])
+        }
+
+        for (i in 1 until heights.size-1) {
+            val minPeak = min(maxWaterLTR[i-1], maxWaterRTL[i+1])
+            if (heights[i] < minPeak) {
+                totalWater += minPeak - heights[i]
+            }
+        }
+
+        return totalWater
+    }
+}
